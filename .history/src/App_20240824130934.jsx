d30c49499fd009ee3c4074affef904e374129c87
metracy <matthew.etracy@gmail.com>
2024-08-24 13:11:40 -0500

    fixed typo for setSat instead of satData

--- .history/src/App_20240824130934.jsx
+++ .history/src/App_20240824130934.jsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 
 
 function App() {
-  const [sat, setSat] = useState(satData);
+  const [sat, satData] = useState(satData);
   //This array holds two variables: sat and setSat. sat will be used to compare changes in state. setSat is a function that will be used to update the state. We set this array equal to the useState function. useState is passed satData. You will use these in the other components.
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   return (
