d3d479504464fa36503248ad0f15d7e4f84ce890
metracy <matthew.etracy@gmail.com>
2024-08-24 16:11:16 -0500

    imported css into all components

---
+++ .history/src/components/Table_20240824160610.jsx
@@ -0,0 +1,38 @@
+import "./styling.css";
+
+let active = (operational) => {
+  if (operational == true) {
+    return "Active"
+  }
+  if (operational == false) {
+    return "Inactive"
+  }
+}
+
+const Table = ({ sat }) => {
+  return (
+    <table>
+    <thead>
+     <tr>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
+     </tr>
+     </thead>
+     <tbody>
+    
+    {sat.map((data) => (
+        <tr key={data.id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{active(data.operational)}</td>
+        </tr>
+    ))}
+     </tbody>
+   </table>
+  )
+};
+
+export default Table;
